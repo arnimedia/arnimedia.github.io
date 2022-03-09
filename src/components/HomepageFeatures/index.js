bd0b17fccc8d04225ba89ed0e7b6c49096eaba28
Arnis Pridans <arnis.pridans@redactive.co.uk>
2022-03-09 21:29:37 +0000

    AP-1 Styling and colors

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -4,7 +4,7 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: '1Vision',
+    title: 'Vision',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
